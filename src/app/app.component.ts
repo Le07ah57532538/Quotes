8c34f56332fae64c1997815c6d5c7d4790041b05
Leah Gakii <leahgakii@gmail.com>
2019-03-25 16:40:49 +0300

    quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,13 @@ import { Quote } from './quote';
 })
 
 export class AppComponent {
-  quotes = [
-      new Quote(1,'Be a big success!We all pray for','been there',new Date(2019,1,7)),
-      new Quote(2,'Buy Cookies','so sweet',new Date(2019,1,7)),
-      new Quote(3, 'Get new Phone Case','new me',new Date(2019,1,7)),
-      new Quote(4, 'Get Dog Food','distgusting',new Date(2019,1,7)),
-      new Quote(5, 'Solve math homework','oops wrong',new Date(2019,1,7)),
-      new Quote(6, 'Plot my world domination plan','too cool',new Date(2019,1,7)),
+  // quotes = [
+  //     new Quote(1,'Be a big success!We all pray for','been there',new Date(2019,1,7)),
+  //     new Quote(2,'Buy Cookies','so sweet',new Date(2019,1,7)),
+  //     new Quote(3, 'Get new Phone Case','new me',new Date(2019,1,7)),
+  //     new Quote(4, 'Get Dog Food','distgusting',new Date(2019,1,7)),
+  //     new Quote(5, 'Solve math homework','oops wrong',new Date(2019,1,7)),
+  //     new Quote(6, 'Plot my world domination plan','too cool',new Date(2019,1,7)),
 
   ]
 constructor(){
