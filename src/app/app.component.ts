523a0a48c9871e1523664fa37a35da19b6589e4e
Leah Gakii <leahgakii@gmail.com>
2019-03-25 16:45:57 +0300

    selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-// import { Component } from '@angular/core';
-// import { Quote } from './quote';
+import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 
 export class AppComponent {
   quotes = [
