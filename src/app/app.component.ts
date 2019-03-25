66dd3136585dc6bde601e37bb718e163876b7757
Leah Gakii <leahgakii@gmail.com>
2019-03-25 16:41:37 +0300

    selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,20 @@ import { Component } from '@angular/core';
 import { Quote } from './quote';
 
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 
 export class AppComponent {
-  // quotes = [
-  //     new Quote(1,'Be a big success!We all pray for','been there',new Date(2019,1,7)),
-  //     new Quote(2,'Buy Cookies','so sweet',new Date(2019,1,7)),
-  //     new Quote(3, 'Get new Phone Case','new me',new Date(2019,1,7)),
-  //     new Quote(4, 'Get Dog Food','distgusting',new Date(2019,1,7)),
-  //     new Quote(5, 'Solve math homework','oops wrong',new Date(2019,1,7)),
-  //     new Quote(6, 'Plot my world domination plan','too cool',new Date(2019,1,7)),
+  quotes = [
+      new Quote(1,'Be a big success!We all pray for','been there',new Date(2019,1,7)),
+      new Quote(2,'Buy Cookies','so sweet',new Date(2019,1,7)),
+      new Quote(3, 'Get new Phone Case','new me',new Date(2019,1,7)),
+      new Quote(4, 'Get Dog Food','distgusting',new Date(2019,1,7)),
+      new Quote(5, 'Solve math homework','oops wrong',new Date(2019,1,7)),
+      new Quote(6, 'Plot my world domination plan','too cool',new Date(2019,1,7)),
 
   ]
 constructor(){
